1725956013f8a257577f406440fcc46f8d9bda65
zues <83701020+bhagath89@users.noreply.github.com>
2021-07-04 07:59:48 +0530

    Update __google.py

--- LaylaRobot/modules/__google.py
+++ LaylaRobot/modules/__google.py
@@ -286,6 +286,5 @@ __help__ = """
  ❍ /github <username>*:* Get information about a GitHub user.
  ❍ /country <country name>*:* Gathering info about given country
  ❍ /imdb <Movie name>*:* Get full info about a movie with imdb.com
- ❍ Yone <query>*:* Yone answers the query
-  💡Ex: `Yone where is India?`
+  💡Ex: `Kerala where is India?`
 """
